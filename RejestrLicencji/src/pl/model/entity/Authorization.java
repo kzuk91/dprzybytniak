f20dc994ee36d61f3dc6cfb7d267825ed34d5e45
Daniel Przybytniak <daniel.przybytniak@student.wat.edu.pl>
2017-05-06 17:37:23 +0200

    Add new entity Authorization

---
+++ RejestrLicencji/src/pl/model/entity/Authorization.java
@@ -0,0 +1,38 @@
+package pl.model.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Danie on 5/6/2017.
+ */
+@Entity
+public class Authorization {
+
+    private String id;
+    private String name;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
