dbbccf80d837f1874cc91cb2f7d6bcdf5b3a2f29
Daniel Przybytniak <daniel.przybytniak@student.wat.edu.pl>
2017-05-07 19:55:44 +0200

    New entity - add new license

---
+++ RejestrLicencji/src/NewLicense.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * Created by Danie on 5/7/2017.
+ */
+public class NewLicense {
+    private JTextField licenseNameTextField;
+    private JTextField licenseIDTextField1;
+    private JTextField instalationTimeTextField;
+    private JTextField licenseDescriptionTextField;
+    private JTextField versionTextField;
+    private JComboBox comboBox1;
+    private JScrollBar scrollBar1;
+    private JTextField instalationStatusTextField;
+    private JTextField validFromTextField;
+    private JTextField validToTextField;
+    private JTextField versionTextField1;
+}
